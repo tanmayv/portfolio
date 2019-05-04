185a0edd16b9d6c1144627a83305ce9fc3e13abf
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2019-05-04 05:45:16 +0530

    DockerFile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="bg-container">
+        <div className="title">Tanmay Vijayvargiya</div>
+        <div className="subtitle">Enthusiastic developer with a knack for problem solving and learn new things.</div>
+        <div className="read-more">
+          <div>Read More</div>
+          <div>^</div>
+        </div>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Work in Progress. Come back later!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
